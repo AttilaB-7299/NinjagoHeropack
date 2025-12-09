26c32ed21f6cfd77aa9fde47fa4716195babee90
NinjagoJay7299 <attilabloom7299@gmail.com>
2025-12-09 10:29:02 -0500

    gave cole jungle item textures

--- assets/nin/renderers/heroes/cole_jungle.js
+++ assets/nin/renderers/heroes/cole_jungle.js
@@ -8,3 +8,7 @@ loadTextures({
     "spinjitzu": "nin:cole/spinjitzu_cole",
     "airjitzu":"nin:cole/airjitzucole"
 });
+function init(renderer) {
+    parent.init(renderer);
+    renderer.setItemIcons("cole/cole_jungle_0", "cole/cole_jungle_1", "cole/cole_jungle_2", "cole/cole_jungle_3");
+}
