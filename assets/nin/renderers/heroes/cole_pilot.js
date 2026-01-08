2678367ee78a4e73683e3c7fb8037a0a88ecc473
NinjagoJay7299 <attilabloom7299@gmail.com>
2026-01-08 10:08:55 -0500

    removed beam from cole pilot earthspikes

--- assets/nin/renderers/heroes/cole_pilot.js
+++ assets/nin/renderers/heroes/cole_pilot.js
@@ -1,4 +1,4 @@
-extend("nin:cole_basic");
+extend("nin:cole_zx");
 loadTextures({
     "layer1": "nin:cole/pilot",
     "layer2": "nin:cole/pilot",
@@ -6,7 +6,10 @@ loadTextures({
     "scythe": "nin:cole/scythe",
     "null": "nin:null",
     "spinjitzu": "nin:cole/spinjitzu_cole",
-    "airjitzu":"nin:cole/airjitzucole"
+    "airjitzu": "nin:cole/airjitzucole",
+    "spikes": "nin:cole/dirttexture",
+    "soq_back": "nin:cole/soq_texture",
+    "arms": "nin:cole/cole_arms",
 });
 function isGolden(entity) {
     return true;
